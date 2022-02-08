bb842ca2a67c401ba371d3d371530fd271ce431e
[alaaelzohary] <[lola_elzohary58@yahoo.com]>
2022-02-08 23:23:08 +0200

    create footer vue page

---
+++ src/views/footer.vue
@@ -0,0 +1,21 @@
+<template>
+  <section>
+    <Section1/>
+    <Section2/>
+
+  </section>
+</template>
+
+<script>
+// @ is an alias to /src
+import Section1 from '@/components/footer/Section1.vue'
+import Section2 from '@/components/footer/Section2.vue'
+export default{
+  name: 'Footer',
+  components: {
+    Section1,
+    Section2
+
+  }
+}
+</script>
